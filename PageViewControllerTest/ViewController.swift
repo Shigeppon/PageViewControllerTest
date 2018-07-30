6d37c30a1297436a144a9720e32822e479cc76ff
Shigeo Sakamoto <shigeo.sakamoto@gmail.com>
2018-07-30 21:07:54 +0900

    Initial Commit

---
+++ PageViewControllerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageViewControllerTest
+//
+//  Created by Shigeo Sakamoto on 2018/07/30.
+//  Copyright © 2018年 Shigeo Sakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
